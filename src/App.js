7e618090a1639a0757ce1ef393b93556346cc130
irene <>
2021-09-26 15:49:14 +0900

    - Add todo files - Update icon url

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="todo-app">
+        <header>
+          <h1 className="todo-app__header">todos</h1>
+          <input type="text"
+                 className="todo-app__new-todo"/>
+          <button/>
+        </header>
+        <div className="todo-app__main">
+          <ul className="todo-list">
+            <li className="todo-item">
+              <button className="toggle"/>
+              <div className="todo-item__view">
+                <div className="todo-item__view__text">text</div>
+                <button className="todo-item__destroy"/>
+              </div>
+              <input type="text"
+                     className="todo-item__edit"/>
+            </li>
+          </ul>
+        </div>
+        <footer className="footer">
+          <span className="todo-count">
+            <strong>1</strong>
+              <span>item</span>
+            left
+          </span>
+          <ul className="todo-filters">
+            <li><a href="#none">All</a></li>
+            <li><a href="#none">Active</a></li>
+            <li><a href="#none">Completed</a></li>
+          </ul>
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
