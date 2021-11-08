d50a91578354e5d4180b4b5ea6a52ed415780e4c
ireneworks <hello@ireneworks.com>
2021-11-08 22:39:53 +0900

    update filter todo

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import "./App.css";
 import Todo from "./components/Todo";
 import TodoForm from "./components/TodoForm";
 
+//저장되있는 투두를 필터에 맞춰서 필터를 해줘야한다.
+//스프레드 문법 안에 저 내용을 추가한다는건지?
+//useMemo가 값을 저장하고 있다가 보여준다라는건지?
+//!isCompleted가 false인데 왜 반대로 보여줘야하지?
+
 export default function App() {
   const [filter, setFilter] = useState("all");
   const [todos, setTodo] = useState([
@@ -47,19 +52,21 @@ export default function App() {
     );
   };
 
-  const filterTodos = useMemo(() => {
-    return todos.filter((todo) => {
-      switch (filter) {
-        case "active":
-          return !todo.isCompleted;
-        case "completed":
-          return todo.isCompleted;
-        default:
-          return todo;
+  const filteredTodos = useMemo(() => {
+    const a = todos.filter((todo) => {
+      if (filter === "active") {
+        return !todo.isCompleted;
+      } else if (filter === "completed") {
+        return todo.isCompleted;
+      } else {
+        return todo;
       }
     });
+    console.log(a);
+    return a;
   }, [todos, filter]);
 
+  //filteredTodos는 filter된 todos
   return (
     <div className="todo-app">
       <header>
@@ -68,7 +75,7 @@ export default function App() {
       </header>
       <div className="todo-app__main">
         <ul className="todo-list">
-          {todos.map((todo) => (
+          {filteredTodos.map((todo) => (
             <Todo
               todo={todo}
               key={todo.id}
@@ -76,18 +83,19 @@ export default function App() {
               onUpdate={onUpdate}
             />
           ))}
+          //todos 어레이를 복사해서 거기에 변경된거를 todos 어레이로 다시
+          넣는걸로
         </ul>
       </div>
       <footer className="footer">
         <span className="todo-count">
-          <strong>{filterTodos.length}</strong>
+          <strong>{filteredTodos.length}</strong>
           <span>item</span>
           left
         </span>
         <ul className="todo-filters">
           <li>
             <a
-              href="#none"
               className={`${filter === "all" ? "selected" : ""}`}
               onClick={() => {
                 setFilter("all");
